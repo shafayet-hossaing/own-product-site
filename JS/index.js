e7cceba422ac01138dcf593290e6177371fa0389
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-14 12:39:14 +0600

    nothing

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+// DOM Selection
+// Click on the add to cart and show the number of order in the badge
+// Click on the add to cart and show the price of the product in the cart
+
+
+
+// Adding the amount of order using function
+// let numberOfOrderCount = 0
+// let totalPriceCount = 0
+// function cart(){
+//     let numberOfOrder = document.querySelector("#numberOfOrder")
+//     let totalOrder = numberOfOrder.innerText = ++numberOfOrderCount;
+//     totalOrder;
+
+
+//     let price = document.querySelector("#price").innerText;
+//     let priceTotalCount = price * numberOfOrderCount
+//     document.querySelector("#totalPrice").innerText = priceTotalCount
+
+//     function price(){
+        
+//     }
+
+// }
+
+
+
+
+const cards = document.getElementsByClassName("card")
+for(const card of cards){
+    const 
+}
